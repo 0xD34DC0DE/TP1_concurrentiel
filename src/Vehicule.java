8b2017f7d84983cd3c293f4c52e66439e67ba9c1
0xD34DC0DE <emerickpoulin31415@gmail.com>
2020-09-17 17:38:34 -0400

    Implemented method charger in class Vehicule

--- src/Vehicule.java
+++ src/Vehicule.java
@@ -1,19 +1,32 @@
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Vehicule {
 
     private int id;
     private LinkedList<Colis> colis;
+    private Route route;
 
     public Vehicule(int id) {
         this.id = id;
     }
 
     public void charger(HashMap<String, Colis> colisALivrer, Route route) {
+        this.route = route;
 
+        List<Colis> colisACharger = colisALivrer
+                .values()
+                .stream()
+                .filter(v -> route.estSurRoute(v.getVille()))
+                .collect(Collectors.toList());
+
+        this.colis.addAll(colisACharger);
+
+        colisACharger.forEach(colis -> colisALivrer.remove(colis.getTracking()));
     }
-
 
     public int getId() {
         return id;
